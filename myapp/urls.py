fadbb7bdeea690354a28b7e0e9cb399e2ebd2739
Joshua Oluwasola <theofficialtjosh@gmail.com>
2023-03-25 23:19:11 +0100

    staticfiles

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,3 +11,7 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('delete/<str:num>', views.delete, name='delete')
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
