c48da4edc0c895ebc5b919e3d5dec46e5e9cc143
Joshua Oluwasola <theofficialtjosh@gmail.com>
2023-03-26 14:00:05 +0100

    env file

--- todolistproject/settings.py
+++ todolistproject/settings.py
@@ -92,12 +92,12 @@ DATABASES = {
 }
 
 #Email forgot password configurations
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tjoshpython@gmail.com'
-EMAIL_HOST_PASSWORD = 'python_16'
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND'),
+EMAIL_HOST = os.environ.get('EMAIL_HOST'),
+EMAIL_PORT = os.environ.get('EMAIL_PORT'),
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS'),
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER'),
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD'),
 
 
 # Password validation
